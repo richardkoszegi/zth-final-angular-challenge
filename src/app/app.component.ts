3385ce216d8916982e015ab7a954d95df04856ba
richardkoszegi <richard.koszegi95@gmail.com>
2018-08-01 10:26:48 +0200

    Feladat 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,4 +21,11 @@ export class AppComponent implements OnInit {
   startCountDown(interval: number) {
     setTimeout(() => alert('Count Down Timeout'), interval * 1000);
   }
+
+  setAlarmClock(alarmDateStr) {
+    const alarmDate =  new Date(Date.parse(alarmDateStr));
+    const now = new Date();
+    const diff: number = alarmDate.getTime() - now.getTime();
+    setTimeout(() => {alert('Alarm Clock Event')}, diff);
+  }
 }
